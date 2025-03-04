b0397678b9c0f4cd9596df5151de6d07204c34f7
SimArch21 <simon.archambault@hotmail.fr>
2025-03-04 21:08:14 +0100

    file wp-config.php well created

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -15,42 +15,18 @@ echo "Granting all privileges on USER"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
 echo "localhost stuff"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -u root -p"admin" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'admin';"
+
+#wordpress and % will have root permissions
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'admin';" 
+mysql -e "SELECT user, host FROM mysql.user;"
 
 echo "flushing privileges"
-mysql -e "FLUSH PRIVILEGES;"
+mysql -e "FLUSH PRIVILEGES;" -p"admin"
 
 echo "shutdowning"
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-echo "maybe this line won't be printed"
+#echo "maybe this line won't be printed"
 exec mysqld_safe
 #exec tail -f 
-#-----------------------------------------------------------------------------o -
-#service mariadb start 
-#mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-#mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-#mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-#mysql -e "FLUSH PRIVILEGES;"
-#mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-#exec mysqld_safe
-#-------------------------------------------------------------------------------
-#service mysqld start
-
-#service mariadb start 
-#echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > db1.sql
-#echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
-#echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> db1.sql
-#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
-#echo "FLUSH PRIVILEGES;" >> db1.sql
-
-#mysql < db1.sql
-
-#kill $(cat /var/run/mysqld/mysqld.pid)
-
-#mysqld
-#-------------------------------------------------------------------------------
-
-#service mariadb stop
-
